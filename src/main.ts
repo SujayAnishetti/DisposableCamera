40d6804f810406e32383859c0e42c5d3a87c2d33
SujayAnishetti <sujayanishetti22@gmail.com>
2025-04-17 05:16:30 +0530

    latest

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { uploadImage } from '../src/uploadcare';
-import { addToQueue, getAllQueued, removeFirstFromQueue } from '../src/db';
+import { uploadImage } from './uploadcare';
+import { addToQueue, getAllQueued, removeFirstFromQueue } from './db';
 
 const video = document.getElementById('camera') as HTMLVideoElement;
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
